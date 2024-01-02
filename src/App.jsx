44f9c49c9d3416e9ea993b3f6d08fb23d5da5e4e
Corriger <rsmoraldo2@gmail.com>
2024-01-02 16:38:23 -0500

    properly changed import 'elements' to real names

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import Content from './components/Header.jsx';
+import Header from './components/Header.jsx';
 import Content from './components/Content.jsx';
-import Content from './components/Total.jsx';
+import Total from './components/Total.jsx';
 
 function App() {
   return (
