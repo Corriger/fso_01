4b743da5610727b3d54133af4499e905f78fb3a0
Corriger <rsmoraldo2@gmail.com>
2024-01-02 16:37:14 -0500

    initial start

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import React from 'react';
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import Content from './components/Header.jsx';
+import Content from './components/Content.jsx';
+import Content from './components/Total.jsx';
 
 function App() {
   return (
     <div>
-      <Header course={course} />
+      <Header />
       <Content />
       <Total />
     </div>
