5858e1978dad91957d0e4ce6ba87441f38ef155c
Corriger <rsmoraldo2@gmail.com>
2024-01-06 13:51:52 -0500

    exercise 1.3 to 1.5

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,26 @@ import Content from './components/Content.jsx';
 import Total from './components/Total.jsx';
 
 function App() {
+  const course = 'Half Stack application development'
+const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   return (
     <div>
-      <Header />
-      <Content />
-      <Total />
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 }
