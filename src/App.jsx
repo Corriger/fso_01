ab794a39f2da80df75f1e95c905704ceb5c1ffef
Corriger <rsmoraldo2@gmail.com>
2024-01-08 17:16:32 -0500

    step 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,38 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <section>
+      <button 
+        onClick={() => setGood(good + 1)}>
+        Good
+      </button>
+      <button 
+        onClick={() => setNeutral(neutral + 1)}>
+        Neutral
+      </button>
+      <button onClick={() => setBad(bad + 1)}>
+        Bad
+      </button>
+        </section>
+      <ul style={{
+          fontSize: `16px`,
+          fontWeight: `bolder`
+        }}>
+        <li style={{
+          color: `green`,
+        }}>{good}</li>
+        <li  style={{
+          color: `grey`,
+        }}>{neutral}</li>
+        <li style={{
+          color: `red`,
+        }}>{bad}</li>
+      </ul>
+      <h2>statistics</h2>
     </div>
   )
 }
---
+++ src/components/Stats.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Stats = (props) => {
+  return (
+    <>
+      <ul>
+        <li text="good" value ={props} />
+        <li text="neutral" value ={} />
+        <li text="bad" value ={} />
+      </ul>
+    </>
+  )
+}
+
+export default Stats
